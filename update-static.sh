8ff5e6b8ccf9bdd3a2917ad9f2e6712c6f28ac49
Ryan Kaiser <ryank@alphageek.xyz>
2016-09-20 20:53:28 -0500

    update favicon prefix

--- update-static.sh
+++ update-static.sh
@@ -2,6 +2,7 @@
 
 . /usr/local/bin/virtualenvwrapper.sh &&
         workon agcs && cdproject && {
-    ./manage.py generate_favicon --prefix 'assets/img/favicon/' agcs/static/img/agcs.png
-    ./manage.py collectstatic --no-input && ./manage.py minify
+    ./manage.py generate_favicon --prefix 'img/favicon/' agcs/static/img/agcs.png
+    ./manage.py collectstatic --no-input #&& ./manage.py minify
 }
+
