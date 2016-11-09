4e915d18aa4d179715b5eb3cfeb11048ac87c5e9
Ryan Kaiser <ryank@alphageek.xyz>
2016-11-08 19:20:55 -0500

    Use API in mailer

--- mailer.py
+++ mailer.py
@@ -4,8 +4,10 @@ import os, sys
 import socket
 import argparse
 from smtplib import SMTP_SSL as SMTP
-from email.mime.text import MIMEText
 from email.utils import formatdate
+from email.message import EmailMessage
+from email.headerregistry import Address
+from email.utils import make_msgid
 
 
 def get_host_name():
@@ -18,13 +20,14 @@ def get_host_name():
 
 class Mailer(object):
     def __init__(self,
-        server, user, passwd, to,
-        content, sender=None, subject=None
+        server, user, passwd, to, content,
+        html=None, sender=None, subject=None
     ):
         self.server = server
         self.user = user
         self.passwd = passwd
         self.content = content
+        self.html = html
         self.to = to
         self.sender = sender or self.user
         self.subject = subject or (
@@ -33,18 +36,26 @@ class Mailer(object):
 
     def send(self):
         try:
-            msg = MIMEText(self.content, 'plain', 'utf-8')
+            msg = EmailMessage()
             msg['Subject'] = self.subject
-            msg['From'] = self.sender
-            msg['To'] = ', '.join(self.to)
+            msg['From'] = Address('Alpha Geeks', *self.sender.split('@'))
+            msg['To'] = tuple(
+                Address('System Admin', *t.split('@'))
+                    for t in self.to
+            )
             msg['Date'] = formatdate()
-            con = SMTP(self.server)
-            con.set_debuglevel(False)
-            con.login(self.user, self.passwd)
-            try:
-                con.sendmail(self.sender, self.to, msg.as_string())
-            finally:
-                con.quit()
+            msg.set_content(self.content)
+            if self.html is not None:
+                msg.add_alternative("""
+                <html><head>
+                <title>{title}</title>
+                </head><body>
+                <pre>{body}</pre>
+                </body></html>
+                """.format(title=self.subject, body=self.html), subtype='html')
+            with SMTP(self.server) as con:
+                con.login(self.user, self.passwd)
+                con.send_message(msg)
         except Exception as e:
             sys.exit("mail failed: %s" % str(e))
 
@@ -72,13 +83,18 @@ if __name__ == '__main__':
         help='Be verbose',
         action='store_true',
     )
-    parser.add_argument('-T', '--test',
+    parser.add_argument('-n', '--dry-run',
         help='Run in test mode',
         action='store_true',
+    )
+    parser.add_argument('-H', '--html',
+        help='Attach html alternative',
+        metavar='HTML'
     )
     parser.add_argument('-s', '--subject',
         help='Subject of the email',
         metavar='SUBJECT',
+        default='System message from %s' % get_host_name()
     )
     parser.add_argument('-S', '--server',
         help='SMTP server to use',
@@ -107,7 +123,7 @@ if __name__ == '__main__':
         nargs='+',
     )
 
-    if ('-T' or '--test') in sys.argv:
+    if ('-n' or '--dry-run') in sys.argv:
         test_args = []
         for k,v in defaults.items():
             test_args.append('--%s' % k)
@@ -118,18 +134,18 @@ if __name__ == '__main__':
         import_args.extend(sys.argv[1:])
 
     args = parser.parse_args(import_args)
-    text = ' '.join(args.message)
-    mail = Mailer(
-        server=args.server,
-        user=args.user,
-        passwd=args.password,
-        to=args.to,
-        content=text,
-        sender=args.user,
-        subject=args.subject or None
-    )
 
     try:
+        mail = Mailer(
+            server=args.server,
+            user=args.user,
+            passwd=args.password,
+            to=args.to,
+            content=' '.join(args.message),
+            html=args.html,
+            sender=args.user,
+            subject=args.subject or None
+        )
         mail.send()
     except Exception as e:
         print("Error: {}".format(e))
