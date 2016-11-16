be1886a721ffb7435e52cea9a9eac85f99e1d90c
Ryan Kaiser <ryank@alphageek.xyz>
2016-11-16 03:55:51 -0500

    Fix typo

--- pgdumpall.sh
+++ pgdumpall.sh
@@ -6,7 +6,7 @@ test `whoami` = root || {
 }
 
 set -e
-FNAME="{1:-agcs_db_all.sql}"
+FNAME="${1:-agcs_db_all.sql}"
 sudo -iu postgres /usr/bin/pg_dumpall -c --if-exists > "$FNAME"
 gzip --rsyncable -n "$FNAME"
 chmod 0400 "${FNAME}.gz"
