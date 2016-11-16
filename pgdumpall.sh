8f713bdb71ab121ea6b8d75917e50a0e9446815e
Ryan Kaiser <ryank@alphageek.xyz>
2016-11-16 03:44:24 -0500

    Fix dumpall

--- pgdumpall.sh
+++ pgdumpall.sh
@@ -5,8 +5,8 @@ test `whoami` = root || {
     exit 1
 }
 
-FNAME="agcs_db_all.sql"
 set -e
-sudo -iu postgres /usr/bin/pg_dumpall -c --if-exists > "${FNAME:=${1:-agcs_db_all.sql}"
+FNAME="{1:-agcs_db_all.sql}"
+sudo -iu postgres /usr/bin/pg_dumpall -c --if-exists > "$FNAME"
 gzip --rsyncable -n "$FNAME"
 chmod 0400 "${FNAME}.gz"
