3a23f6ca72195958f0b392d201a78667f93f9aa5
Ryan Kaiser <ryank@alphageek.xyz>
2016-11-23 21:16:46 -0500

    Restore from .save

--- pgdumpall.sh
+++ pgdumpall.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-test `whoami` = root || {
-    echo 'Must be root'
+err () {
+    echo "Usage: ${BASH_SOURCE##*/} [OUTFILE]"
+    echo ${@:--ne}
     exit 1
 }
 
+trap err ERR
 set -e
+test `whoami` = root || err 'Must be root'
 FNAME="${1:-agcs_db_all.sql}"
-sudo -iu postgres /usr/bin/pg_dumpall -c --if-exists > "$FNAME"
+sudo -iu postgres /usr/bin/pg_dumpall -c --if-exists >"$FNAME"
 gzip --rsyncable -n "$FNAME"
 chmod 0400 "${FNAME}.gz"
