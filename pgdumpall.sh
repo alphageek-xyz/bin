c7c84386004066184c4a38d521849d8eb6ab1590
Ryan Kaiser <ryank@alphageek.xyz>
2016-11-23 23:29:34 -0500

    Check for and delete file on error

--- pgdumpall.sh
+++ pgdumpall.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 err () {
+    [[ -n "$FNAME" ]] && [[ -f "$FNAME" ]] &&
+        rm -vf "$FNAME"{,.gz} || true
     echo "Usage: ${BASH_SOURCE##*/} [OUTFILE]"
     echo ${@:--ne}
     exit 1
 }
 
+exec >&2
 trap err ERR
+
 set -e
 test `whoami` = root || err 'Must be root'
 FNAME="${1:-agcs_db_all.sql}"
